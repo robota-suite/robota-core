c2e7a19e4a705bfc2509e9cf4cc0300d2e507a68
Peter Crowther <peter.crowther-3@manchester.ac.uk>
2021-03-30 01:21:36 +0100

    Bump version numbers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Peter Crowther'
 author = 'Peter Crowther'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.1.0'
 
 
 # -- General configuration ---------------------------------------------------
